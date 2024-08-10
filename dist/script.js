afcab663c1871c721a3f8ea5997e7ecbbebf211f
b-IXn <palindrome106@gmail.com>
2024-08-10 13:55:02 +0545

    nepthailo

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+const navDialog = document.getElementById('nav-dialog');
+function openMenu() {
+    navDialog.classList.toggle('hidden')
+    
+}
+// function setupObserver(element,isLTR,speed){
+//     const intersectionObserver = new IntersectionObserver
+// }
